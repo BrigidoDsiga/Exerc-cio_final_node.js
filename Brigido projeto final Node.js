09a67c1b3ad2b36f3d507e566a39a772ba688cf3
Brígido Domingos Siga <103961163+BrigidoDsiga@users.noreply.github.com>
2022-11-11 13:17:20 -0300

    Update Brigido projeto final Node.js

--- Brigido projeto final Node.js
+++ Brigido projeto final Node.js
@@ -1,1 +1,9 @@
+Exercícico Node.js
+O servidor express e Sqlite3 são executados utilizando o Node.js instalados no seu computador.
 
+Iniciando o projeto
+npm install
+npm run dev
+Rode as migrações
+Rodando as migrações de banco de dados: npx sequelize-cli db:migrate
+Criando registros com seeds: npx sequelize-cli db:seed:all
